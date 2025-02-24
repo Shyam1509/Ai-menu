7f32d03ae4475d8e8e608ca249f837c7d901b9c9
Ghanshyam Mahajan <mahajanghanshyam86@gmail.com>
2025-02-24 21:10:03 +0530

    bug resolve

--- ai-menu-backend/server.js
+++ ai-menu-backend/server.js
@@ -13,13 +13,13 @@ server.use(express.json());
 server.use(cors({ origin: "*" })); // Allow all origins (for testing)
 
 
-// Connect Database
-connectDB();
-
 // Routes
 server.use("/menu", menuRoutes);
 server.use("/generate-qr", generateqr);
 
 
 // Start Server
-server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+server.listen(PORT, () => 
+    console.log(`🚀 Server running on port ${PORT}`),
+    connectDB()
+);
